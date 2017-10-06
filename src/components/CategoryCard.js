a5db178196fc008ff55c5893281189b88ca584ee
Alexandra <alexandra@webcrumbz.co>
2017-10-06 16:11:01 +0300

    Fix category card alignment

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,26 +1,22 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
-import { Card, Image, Button, Grid, Header } from 'semantic-ui-react';
+import { Card, Image, Button, Header } from 'semantic-ui-react';
 
 class CategoryCard extends Component {
   render() {
     return (
       <Card raised centered>
         <Card.Content>
-          {this.props.src !== '' ? <Image floated="left" size="tiny" shape="circular" src={this.props.src} /> : null}
-          <Grid>
-            <Grid.Row>
-              <Card.Header as={Header}>{this.props.name}</Card.Header>
-            </Grid.Row>
-            <Grid.Row>
-              <Link to={'/category/' + this.props.categId}>
-                <Button color="purple" compact>
-                  View Products &gt;
-                </Button>
-              </Link>
-            </Grid.Row>
-          </Grid>
+          {this.props.src !== '' ? (
+            <Image floated="left" size="tiny" shape="circular" src={this.props.src} />
+          ) : null}
+          <Card.Header as={Header}>{this.props.name}</Card.Header>
+          <Link to={'/category/' + this.props.categId}>
+            <Button color="purple" compact>
+              View Products &gt;
+            </Button>
+          </Link>
         </Card.Content>
       </Card>
     );
