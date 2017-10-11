e208f6e03c0f9fd31210e2fd387131c6af29ccda
Alexandra Anghel <anghelalexandra@users.noreply.github.com>
2017-10-11 11:50:53 +0300

    Image slider for product photo gallery (#4)
    * Add image gallery package
    
    * Add image gallery to product details
    
    * Increase image gallery slide duration
    
    * Update image gallery styles
    
    * Move js import before css

--- src/views/Product/ProductDetails.js
+++ src/views/Product/ProductDetails.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Header, Card, Image, Icon, Button } from 'semantic-ui-react';
+import { Header, Card, Icon, Button } from 'semantic-ui-react';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 import { productPropType } from '../Products/reducer';
 import { addProduct } from '../Cart/actions';
 import Rating from '../../components/Rating';
+import './styles.css';
 
 class ProductDetails extends Component {
   constructor(props) {
@@ -16,9 +19,16 @@ class ProductDetails extends Component {
 
   getCategories() {
     return this.props.product.categories.reduce(
-      (categString, category, index) => categString + category.name + (index === this.props.product.categories.length - 1 ? '' : ', '),
+      (categString, category, index) =>
+        categString +
+        category.name +
+        (index === this.props.product.categories.length - 1 ? '' : ', '),
       ' ',
     );
+  }
+
+  getImageGallery() {
+    return this.props.product.images.map(image => ({ original: image.src }));
   }
 
   addItem() {
@@ -32,16 +42,28 @@ class ProductDetails extends Component {
       <div>
         <Header textAlign="center">{this.props.product.name}</Header>
         <Card centered raised>
-          <Image src={this.props.product.images[0].src} />
+          <ImageGallery
+            items={this.getImageGallery()}
+            slideDuration={550}
+            showPlayButton={false}
+            showThumbnails={false}
+          />
           <Card.Content extra>
             {Number(this.props.product.average_rating) === 0 ? (
               <p>No ratings</p>
             ) : (
-              <Rating rating={Number(this.props.product.average_rating)} ratingCount={this.props.product.rating_count} />
+              <Rating
+                rating={Number(this.props.product.average_rating)}
+                ratingCount={this.props.product.rating_count}
+              />
             )}
           </Card.Content>
-          {this.props.product.categories.length === 0 ? null : <Card.Content>Categories: {this.getCategories()}</Card.Content>}
-          <Card.Content>Stock: {this.props.product.in_stock ? 'In Stock' : 'Out of Stock'}</Card.Content>
+          {this.props.product.categories.length === 0 ? null : (
+            <Card.Content>Categories: {this.getCategories()}</Card.Content>
+          )}
+          <Card.Content>
+            Stock: {this.props.product.in_stock ? 'In Stock' : 'Out of Stock'}
+          </Card.Content>
           <Card.Content>Price: {this.props.product.price}$</Card.Content>
           <Button color="purple" fluid onClick={this.addItem}>
             ADD TO CART &nbsp;<Icon name="cart" />
