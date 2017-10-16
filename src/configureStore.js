917223b2d33b933efd8f67726627d691c1614eca
EkoAlex <EkoAlex@users.noreply.github.com>
2017-10-16 15:10:38 +0300

    Persist cart (#7)
    * Add redux persist
    
    * Configure redux persist
    
    * Add skip fetching transform

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { createTransform, persistStore, autoRehydrate } from 'redux-persist';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import createHistory from 'history/createBrowserHistory';
@@ -13,6 +14,7 @@ import cart from './views/Cart/reducer';
 const history = createHistory();
 
 const defaultState = {
+  sideMenuVisible: false,
   categories: {
     items: [],
     isFetching: 0,
@@ -32,7 +34,17 @@ const defaultState = {
 
 const rootReducer = combineReducers({ sideMenuVisible, categories, products, reviews, cart });
 
-const store = createStore(rootReducer, defaultState, applyMiddleware(thunk, logger, routerMiddleware(history)));
+const skipIsFetchingTransform = createTransform((inboundState, key) => {
+  if (key !== 'products' && key !== 'categories' && key !== 'reviews') return inboundState;
+  return {
+    ...inboundState,
+    isFetching: undefined,
+  };
+});
+
+const store = createStore(rootReducer, defaultState, compose(applyMiddleware(thunk, logger, routerMiddleware(history)), autoRehydrate()));
+
+persistStore(store, { blacklist: ['sideMenuVisible'], transforms: [skipIsFetchingTransform] });
 
 export { history };
 export default store;
