62c864ad0dfc371f6b8a04cd9a12cd37094103ba
EkoAlex <alexandru@appticles.com>
2017-10-27 15:18:30 +0300

    Add variations end point

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,7 @@ const GLOBAL_VARIABLES = {
   API_PRODUCTS_URL: 'http://localhost/wp/wp-json/wpmppro/products/',
   API_PRODUCT_URL: 'http://localhost/wp/wp-json/wpmppro/product/',
   API_REVIEWS_URL: 'http://localhost/wp/wp-json/wpmppro/reviews/',
+  API_VARIATIONS_URL: 'http://localhost/wp/wp-json/wpmppro/product-variations/',
   API_CHECKOUT_URL: 'http://localhost/wp/wp-json/wpmppro/proceed-checkout',
   CHECKOUT_URL: 'http://localhost/wp/checkout/',
 };
