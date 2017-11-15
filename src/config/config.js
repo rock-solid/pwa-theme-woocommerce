4e1adcb180ffd428a993f9f63bc3c387a796d5a3
EkoAlex <alexandru@appticles.com>
2017-11-15 18:11:58 +0200

    Update config to get values from window

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,3 @@
-const GLOBAL_VARIABLES = {
-  API_CATEGORIES_URL: 'http://localhost/wp/wp-json/pwacommerce/categories/',
-  API_PRODUCTS_URL: 'http://localhost/wp/wp-json/pwacommerce/products/',
-  API_PRODUCT_URL: 'http://localhost/wp/wp-json/pwacommerce/product/',
-  API_REVIEWS_URL: 'http://localhost/wp/wp-json/pwacommerce/reviews/',
-  API_VARIATIONS_URL: 'http://localhost/wp/wp-json/pwacommerce/product-variations/',
-  API_CHECKOUT_URL: 'http://localhost/wp/wp-json/pwacommerce/proceed-checkout',
-  CHECKOUT_URL: 'http://localhost/wp/checkout/',
-  CURRENCY: '&pound;',
-};
+const GLOBAL_VARIABLES = window.appticles.config;
 
 export default GLOBAL_VARIABLES;
